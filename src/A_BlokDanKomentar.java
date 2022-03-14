7957ba839eef6bef1a2c6bf896df505732d28213
rizkijulian13 <13erje13>
2022-03-14 14:53:27 +0700

    initial project pertemuan 1 dasar pemrograman java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,9 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Dasar Pemograman Java");
+        // contoh komentar 1 baris
+        /*
+       Contoh Komentar
+         */
+    }
+}
